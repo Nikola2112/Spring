c1253018802fa1ab78d075837a9d2e542d0cf1f1
Nikola2112 <nikola21122004@gmail.com>
2023-08-29 23:21:18 +0100

    done

---
+++ src/main/java/com/goit/Spring/Boot/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.goit.Spring.Boot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String hello(){
+      return  "Hello Word";
+    }
+}
