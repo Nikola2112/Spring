9ae1ef503f740b5bab753b5ecaed5db7e2260903
Nikola2112 <nikola21122004@gmail.com>
2023-09-07 13:37:01 +0100

    Data Spring

--- src/main/java/com/goit/Spring/Boot/repository/NoteRepository.java
+++ src/main/java/com/goit/Spring/Boot/repository/NoteRepository.java
@@ -1,17 +1,11 @@
 package com.goit.Spring.Boot.repository;
 
 import com.goit.Spring.Boot.entities.Note;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+
 
 @Repository
-public interface NoteRepository extends CrudRepository<Note,Long>{
-
-    List<Note> findAll();
-    Note save(Note note);
-    void deleteById(Long id);
-    Optional<Note> findById(Long id);
+public interface NoteRepository extends JpaRepository<Note,Long> {
 }
